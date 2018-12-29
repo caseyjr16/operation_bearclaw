512b2f63ae8354f2753d346c2265ebca84e50029
Casey <caseyjr07@sbcglobal.net>
2018-12-29 16:17:34 -0500

    added name textfields

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+
+constructor(props){
+
+  super(props);
+  this.state = {firstname: '',
+lastname: ''};
+
+this.handleChange = this.handleChange.bind(this);
+this.handleChange2 = this.handleChange2.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleChange(event){
+  this.setState({firstname: event.target.value})
+
+}
+
+handleChange2(event){
+  this.setState({lastname: event.target.value})
+  
+}
+
+handleSubmit(event) {
+  alert('A name was sumbitted:' + this.state.firstname + " " + this.state.lastname);
+  event.preventDefault();
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hello, Casey</h1>
+         <form onSubmit={this.handleSubmit}>
+         <label>
+           First Name:
+           <input type="text" value={this.state.firstname} 
+           onChange={this.handleChange} />
+           </label>
+           
+           <label>
+           Last Name: 
+           <input type="text" value={this.state.lastname} onChange={this.handleChange2} />
+           </label>
+           <input type="submit" value="Submit" /> 
+           </form>
+
         </header>
       </div>
     ); 
