5c4920d3579fe2007719842af80c833a370a0dfc
sirsteele97 <mordue.1@osu.edu>
2018-12-29 22:25:21 -0500

    add server

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors())
+
+app.listen(4000, ()=> {
+    console.log('Server is listening of port 4000...')
+})
