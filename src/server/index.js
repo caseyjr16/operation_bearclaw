8bbb92fb779a6014607825e11a5e943bf32d7512
Casey <caseyjr07@sbcglobal.net>
2019-01-03 16:01:52 -0500

    connected to database using GET function

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,49 @@
+
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
+
 
 const app = express();
 
-app.use(cors())
+const SELECT_ALL_USERS_QUERY = 'SELECT * FROM users';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'MyNewPass',
+    database: 'studentAccounts'
+});
+
+
+connection.connect(err => {
+    if(err){
+        return err;
+    }
+});
+
+app.use(cors());
+
+
+console.log(connection)
 
 app.get('/', (req, res) =>{
-    res.send('Hello mason and Casey')
+    res.send('Hello Casey and Mason. Go to /users to see users')
+});
+
+app.get('/users', (req, res) => {
+    connection.query(SELECT_ALL_USERS_QUERY, (err, results) => {
+        if(err){
+            return res.send(err)
+        }
+        else{
+            return res.json({
+                data: results
+            })
+        }
+    });
 });
 
 app.listen(4000, ()=> {
-    console.log('Server is listening of port 4000...')
+    console.log('Server is listening on port 4000...')
 })
